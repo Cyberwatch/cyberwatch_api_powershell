084890db7f3ed72680613ad0585a7478a153c176
Daniel Deutsch <daniel@cyberwatch.fr>
2020-06-10 13:21:34 +0200

    Add upload script result to Importer + example

---
+++ examples/send_results_importer.ps1
@@ -0,0 +1,64 @@
+# -------------------------------------
+# CONFIGURATION
+# Please check and complete these items
+# -------------------------------------
+
+#Cyberwatch API authentication
+$API_KEY = ""
+$SECRET_KEY = ""
+$API_URL = ""
+
+# -------------------------
+# RUN
+# -------------------------
+
+Write-Output "-------------------------------------------"
+Write-Output "Cyberwatch - Send results for analysis"
+Write-Output "-------------------------------------------"
+
+Function SendResultsImporter
+{
+<#
+.SYNOPSIS
+        Example script to send Importer scanning scripts results
+#>
+
+# Create the client variable
+$client = Get-CyberwatchApi -api_url $API_URL -api_key $API_KEY -secret_key $SECRET_KEY
+
+# Test the client connection
+Write-Output "INFO: Checking API connection and credentials..."
+try
+{
+    $uuid = $client.ping().uuid
+    Write-Output "INFO: OK."
+}
+catch
+{
+    Write-Output "ERROR: Connection failed. Please check credentials. If needed, please reach the Cyberwatch support for further assistance."
+    Return
+}
+
+# Load results and send them to Cyberwatch
+Write-Output "INFO: Searching for available results..."
+$available_results = Get-ChildItem -Recurse -File -Path ".\upload"
+Write-Output "INFO: Done. Found $($available_results.count) results to be processed and sent for analysis."
+
+$available_results | % {
+    Write-Output "INFO: Reading $($_.FullName) content..."
+    $content = [IO.File]::ReadAllText($_.FullName)
+    Write-Output "INFO: Sending $($_.FullName) content to the API..."
+    $client.send_result_importer(@{ output = $content })
+    Write-Output "INFO: Done."
+}
+
+
+Write-Output "---------------------------------------------------------------------"
+Write-Output "Script completed!"
+Write-Output "Your scans are now being processed by your Cyberwatch nodes."
+Write-Output "Please log on $API_URL to see the results."
+Write-Output "---------------------------------------------------------------------"
+
+}
+
+SendResultsImporter
