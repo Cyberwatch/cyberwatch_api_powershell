441b237477fd36ae2529d901f8b274ee95c3247d
Daniel Deutsch <daniel@cyberwatch.fr>
2022-01-31 16:02:30 +0100

    Updated Servers CVEs script

--- examples/servers_cves_csv.ps1
+++ examples/servers_cves_csv.ps1
@@ -10,55 +10,43 @@ $servers = $client.servers(@{
     "group_id" = $GROUP_ID
 })
 
-# Get all servers details + all CVE code
+#Get all servers details + all CVE code
 $all_servers = @()
-$all_cves = @()
 foreach ($server in $servers) {
     if ($server.cve_announcements_count -gt 0) {
         $asset = $client.server($server.id)
         $all_servers += $asset
-        foreach ($cve in $asset.cve_announcements) {
-            $all_cves += $cve.cve_code
-        }
     }
-}
-
-# Get all CVE details from all CVE codes
-$all_cves_details = @{}
-foreach($cve in $all_cves | Select-Object -Unique) {
-    $all_cves_details[$cve] = $client.cve_announcement($cve)
 }
 
 # Find corresponding technology of each server + build CSV list
-$to_export = @()
 foreach ($server in $all_servers) {
-    foreach ($cve in $server.cve_announcements) {
-        foreach ($cve_server in $all_cves_details[$cve.cve_code].servers) {
-            if ($cve_server.id -eq $server.id) {
-                if ($null -eq $cve_server.updates[0].target.product) {
-                    $technology = $cve_server.updates[0].current.product
-                }
-                else {
-                    $technology = $cve_server.updates[0].target.product
+    $to_export = @()
+    foreach ($update in $server.updates) {
+        foreach ($update_cve in $update.cve_announcements) {
+            # Find details of each cve from update
+            foreach ($server_cve in $server.cve_announcements) {
+                if ($server_cve.cve_code -eq $update_cve) {
+                    $cve = $server_cve
                 }
             }
-        }
-
-        $to_export += [pscustomobject]@{
-            Hostname     = $server.Hostname
-            Technologie  = $technology
-            Vulnerabilite  = $cve.cve_code
-            Derniere_analyse = $server.analyzed_at
-            Score_CVSS = $cve.score
-            Vulnerabilite_prioritaire = $cve.prioritized
-            Date_de_detection = $cve.detected_at
-            Ignoree = $cve.ignored
-            Commentaire = $cve.comment
+            $to_export += [pscustomobject]@{
+                Hostname     = $server.Hostname
+                Technologie  = $update.target.product
+                Vulnerabilite  = $cve.cve_code
+                Derniere_analyse = $server.analyzed_at
+                Score_CVSS = $cve.score
+                Vulnerabilite_prioritaire = $cve.prioritized
+                Date_de_detection = $cve.detected_at
+                Ignoree = $cve.ignored
+                Commentaire = $cve.comment
+            }
         }
     }
+    $to_export | Sort-Object -Property 'Hostname' -Descending | Export-Csv -NoTypeInformation -Delimiter ';' -Path (join-path ((Get-Item .).FullName) "$(get-date -f dd-MM)_$($server.Hostname)_cyberwatch.csv")
+
 }
 
 # Export CSV
-$to_export | Sort-Object -Property 'Hostname' -Descending | Export-Csv -NoTypeInformation -Delimiter ';' -Path (join-path ((Get-Item .).FullName) "$(get-date -f dd-MM)_export_cyberwatch.csv")
 
 cmd /c pause
